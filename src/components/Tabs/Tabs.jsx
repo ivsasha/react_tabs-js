e8673a29d6b4d252b749d40bc0fee1efb38befdb
Olexandr Ivaschenko <sasaivasenko9@gmail.com>
2025-05-15 15:51:56 +0300

    create tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,15 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTab }) => {
+  return (
+    <div className="block" data-cy="TabContent">
+      {tabs.map(tab => (
+        <div key={tab.id} data-cy="TabContent">
+          {activeTab === tab.id && (
+            <div className="content" data-cy="TabContent">
+              {tab.content}
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
